3052b9a268a3bfdbaf319b3195b07d1dcaeb6aea
eunathmoreira <nathaliamoreira.costa@gmail.com>
2024-10-05 12:45:20 -0300

    Add files via upload

---
+++ exercicio Nathalia Moreira de Abreu da Costa.js
@@ -0,0 +1,24 @@
+function ordemdemaiornumerodemedalhasnasolimpiadas () {
+    const pais = ['brasil', 'frança', 'argentina']
+    const medalhas = ['7', '6','3']
+}
+
+
+
+while (true) {
+    const pais = prompt ("digite o nome do pais (ou 'sair' para terminar):") 
+
+  if (pais.toLowerCase () === "sair" ) 
+    break;
+}
+const medalhas = prompt ("digite o a quantidade de medalhas (ou 'sair' para terminar)")
+
+console.log(pais[0]); 
+console.log(pais[1]);
+console.log(pais[2]);
+
+// tentei desenvolver o maximo baseado nos exercicios mostrados, porem não estou conseguindo dar seguimento, tentei o maximo dentro do tempo estipulado
+// obrigada pela aula e aguardo o retorno .
+
+
+
